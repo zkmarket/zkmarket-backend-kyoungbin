13069b4ee9b4d21d2d16d206f1c3cfac72bd0f5c
kim <kyoungbin1996@gmail.com>
2023-07-12 12:20:34 +0900

    impl : zktransfer snark inputs and bridge

--- server/src/snarks/index.js
+++ server/src/snarks/index.js
@@ -1,17 +1,21 @@
 import RegistDataSnarkInputs from "./inputs/registDataInputs"
 import AcceptTradeSnarkInputs from "./inputs/acceptTradeInputs"
 import GenTradeSnarkInputs from "./inputs/genTradeInputs";
-import LibSnark from "./libsnark"
+import LibSnark, { ZkTransferLibsnark } from "./libsnark"
+import ZkTransferSnarkInputs from "./inputs/zkTransferInputs";
 
 const registDataProver = new LibSnark("RegistData");
 const genTradeProver = new LibSnark("GenTrade");
 const acceptTradeProver = new LibSnark("AcceptTrade");
+const zkTransferProver = new ZkTransferLibsnark();
 
 export default {
     registDataInput : RegistDataSnarkInputs,
     acceptTradeInput: AcceptTradeSnarkInputs,
     genTradeInput : GenTradeSnarkInputs,
+    zkTransferInput : ZkTransferSnarkInputs,
     genTradeProver: genTradeProver,
     registDataProver: registDataProver,
     acceptTradeProver: acceptTradeProver,
+    zkTransferProver: zkTransferProver
 }
--- server/src/snarks/libsnark.js
+++ server/src/snarks/libsnark.js
@@ -11,11 +11,9 @@ export default class LibSnark {
         this.type = verify ?  "verifier" : "prover" ;
         this.CircuitType = circuitType;
 
-        this.pk_file_path = Buffer.alloc(250); this.pk_file_path.write(crsPath+this.CircuitType+'_crs_pk.dat');
-        if(verify){
-            this.vk_file_path = Buffer.alloc(250); this.vk_file_path.write(crsPath+this.CircuitType+'_crs_vk.dat');
-        }
-        const circuit_type_buf = Buffer.alloc(30); circuit_type_buf.write(this.CircuitType);
+        this.pk_file_path = strToBuf(crsPath+this.CircuitType+'_crs_pk.dat')
+        if(verify){this.vk_file_path = strToBuf(crsPath+this.CircuitType+'_crs_vk.dat')}
+        const circuit_type_buf = strToBuf(this.CircuitType, 30);
 
         
         this.contextId = SnarkLib.createCircuitContext(
@@ -30,7 +28,7 @@ export default class LibSnark {
         
         if(verify) SnarkLib.readVK(this.contextId,this.vk_file_path)
         else SnarkLib.readPK(this.contextId, this.pk_file_path);
-        this.getLastFunctionMsg();
+        // this.getLastFunctionMsg();
     }
 
     uploadInputJsonStr(snarkInputJson){
@@ -80,4 +78,60 @@ export default class LibSnark {
             return undefined;
         }
     }
+}
+
+export class ZkTransferLibsnark{
+    contextId = 0;
+    circuitName = 'ZKlay';
+    serializeFormat = 'SERIALIZE_FORMAT_ZKLAY';
+    treeHeight = '32';
+    hashType = 'MiMC7';
+    ecSelection = Config.EC_ALT_BN128;
+    
+    constructor(circuitName='ZKlay', treeHeight = '32', hashType = 'MiMC7', serializeFormat = 'SERIALIZE_FORMAT_ZKLAY') {
+        this.circuitName = circuitName;
+        this.treeHeight = treeHeight;
+        this.hashType = hashType;
+        this.serializeFormat = serializeFormat;
+
+        this.contextId = SnarkLib.createCircuitContext(
+            strToBuf(this.circuitName), 
+            Number(Config.R1CS_GG),
+            Number(Config.EC_ALT_BN128),
+            null, null, null
+        );
+        console.log('zktransfer contextId : ', this.contextId, typeof(this.contextId))
+        
+        SnarkLib.serializeFormat(this.contextId, Number(Config.serializeFormat))
+        SnarkLib.assignCircuitArgument(this.contextId, strToBuf('treeHeight'), strToBuf(this.treeHeight));
+        SnarkLib.assignCircuitArgument(this.contextId, strToBuf('hashType'), strToBuf(this.hashType));
+        SnarkLib.buildCircuit(this.contextId);
+        
+        SnarkLib.readPK(this.contextId, strToBuf(crsPath+this.circuitName+'_crs_pk.dat'));
+    }
+
+    uploadInputJsonStr(snarkInputJsonStr){
+        const buf=strToBuf(snarkInputJsonStr, snarkInputJsonStr.length*2);
+        SnarkLib.updatePrimaryInputFromJson(this.contextId, buf);
+    }
+
+    runProof(proofId = ""){
+        SnarkLib.runProof(this.contextId); 
+        SnarkLibUtils.write_proof_json(this.contextId, snarkPath+this.circuitName+proofId+'_proof.json');
+    }
+
+    uploadInputAndRunProof(snarkInputJsonStr, proofId=""){
+        this.uploadInputJsonStr(snarkInputJsonStr);
+        this.runProof(proofId);
+    }
+
+    getLastFunctionMsg(){
+        console.log(SnarkLib.getLastFunctionMsg(this.contextId));
+    }
+}
+
+const strToBuf = (str, size=256) => {
+    const buf = Buffer.alloc(size);
+    buf.write(str);
+    return buf;
 }
